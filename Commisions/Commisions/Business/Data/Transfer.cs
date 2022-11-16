15c87be970f408e417f34255c01e950e4eea5213
Michał Janas <michal.janas1@outlook.com>
2022-11-16 05:44:33 +0100

    reprezentacja uzytkownikow i przelewow

---
+++ Commisions/Commisions/Business/Data/Transfer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Commisions.Business.Data
+{
+    public class Transfers
+    {
+        public Transfers()
+        {
+            List = new List<Transfer>();
+        }
+
+        public List<Transfer> List { get; set; }
+    }
+    public class Transfer
+    {
+        public int SourceId { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ Commisions/Commisions/Business/Data/User.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Commisions.Business.Data
+{
+    public class Users
+    {
+        public List<User> List { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public int SubordinatesCount { get; set; } // who dont have subordinates
+        public int SupervisorId { get; set; }
+        public int Commision { get; set; }
+        public bool HasSuboridantes { get; set; } = true;
+
+        public void AddAmount(int amount)
+        {
+            Commision += amount;
+        }
+
+        override
+        public string ToString()
+        {
+            return $"{Id} {Level} {SubordinatesCount} {Commision}";
+        }
+    }
+}
