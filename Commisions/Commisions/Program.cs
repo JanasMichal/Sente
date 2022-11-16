36fa796c890b9b6f2612fc17fe41a3772828dd33
Michał Janas <michal.janas1@outlook.com>
2022-11-16 03:33:50 +0100

    first commit

---
+++ Commisions/Commisions/Program.cs
@@ -0,0 +1,26 @@
+﻿using Commisions.Business;
+using Commisions.Business.Contract;
+using Commisions.Business.Misc;
+using Commisions.Business.Processors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Commisions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IProcessor, UsersDataProcessor>()
+                .AddSingleton<IProcessor, TransferDataProcessor>()
+                .AddSingleton<IXmlHelper, UsersXmlHelper>()
+                .AddSingleton<IXmlHelper, TransfersXmlHelper>()
+                .AddSingleton<IManagment, Managment>()
+                .BuildServiceProvider();
+
+            var manage = serviceProvider.GetService<IManagment>();
+
+            manage.Run();
+        }
+    }
+}
